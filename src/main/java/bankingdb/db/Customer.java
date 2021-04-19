c5e84be49a246ca59141627e58bd757e7e9dd3be
unknown <rcakshat@gmail.com>
2021-04-19 11:19:08 +0530

    Added lombok to entities

--- src/main/java/bankingdb/db/Customer.java
+++ src/main/java/bankingdb/db/Customer.java
@@ -1,14 +1,17 @@
 package bankingdb.db;
 
+import lombok.Data;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+
 //using JPA for object relation mapping
 @Table(name="Accounts")
 @Entity
-public class Customer {
+public @Data class Customer {
     private @Id long id;
     private @Column String firstname;
     private @Column String secondname;
@@ -20,49 +23,8 @@ public class Customer {
         this.id=id;
         this.firstname=firstname;
         this.secondname=secondname;
-        this.setBalance(balance);
+        this.balance=balance;
 
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getFirstname() {
-        return firstname;
-    }
-
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
-    public String getSecondname() {
-        return secondname;
-    }
-
-    public void setSecondname(String secondname) {
-        this.secondname = secondname;
-    }
-
-    public double getBalance() {
-        return balance;
-    }
-
-    public void setBalance(double balance) {
-        this.balance = balance;
-    }
-
-    @Override
-    public String toString() {
-        return "Customer{" +
-                "id=" + id +
-                ", firstname='" + firstname +
-                ", secondname=" + secondname +
-                ", balance=" + getBalance() +'}';
     }
 
 }
--- src/main/java/bankingdb/db/Transact.java
+++ src/main/java/bankingdb/db/Transact.java
@@ -1,10 +1,12 @@
 package bankingdb.db;
 
+import lombok.Data;
+
 import javax.persistence.*;
 
 @Table(name="Transactions")
 @Entity
-public class Transact {
+public @Data class Transact {
     private @Id @GeneratedValue long tranid;
     private @Column long accid;
     private @Column double transferbalance;
@@ -13,42 +15,11 @@ public class Transact {
     public Transact(){}
 
     public Transact(long accid,long tranId,double transferBalance,boolean InOut) {
-        this.setAccid(accid);
-        this.setTranId(tranId);
-        this.setTransferBalance(transferBalance);
-        this.setInOut(InOut);
+        this.accid=accid;
+        this.tranid=tranId;
+        this.transferbalance=transferBalance;
+        this.inout=InOut;
     }
 
 
-    public long getTranId() {
-        return tranid;
-    }
-
-    public void setTranId(long tranid) {
-        this.tranid = tranid;
-    }
-
-    public long getAccid() {
-        return accid;
-    }
-
-    public void setAccid(long accid) {
-        this.accid = accid;
-    }
-
-    public double getTransferBalance() {
-        return transferbalance;
-    }
-
-    public void setTransferBalance(double transferbalance) {
-        this.transferbalance = transferbalance;
-    }
-
-    public boolean getInOut() {
-        return inout;
-    }
-
-    public void setInOut(boolean inout) {
-        this.inout = inout;
-    }
 }
